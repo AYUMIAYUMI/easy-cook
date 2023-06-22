58e5be6319efa9002835324b928cd698fa23be1e
AYUMIAYUMI <ha.jd.12b@gmail.com>
2023-06-22 20:46:28 +0900

    ニックネーム文字数変更

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,4 +8,5 @@ class Recipe < ApplicationRecord
   
   has_one_attached :image
   belongs_to :user
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,  password_length: 6..10
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with:  VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'
+  validates :nick_name, presence: true, length: { maximum: 10 }
+  
+
+
+  has_many :recipes
 end
