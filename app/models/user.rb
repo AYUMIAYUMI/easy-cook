3725d0fbb9277ccc61d94e9bda8143c43e17120f
AYUMIAYUMI <ha.jd.12b@gmail.com>
2023-06-28 21:37:38 +0900

    テストコード完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,  password_length: 6..10
+         :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with:  VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'
-  validates :nick_name, presence: true, length: { maximum: 10 }
+  validates :nick_name, presence: true
   
 
 
