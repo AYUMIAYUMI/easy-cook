1dc847ede9ac9d6514da746023318e06783aba8f
AYUMIAYUMI <ha.jd.12b@gmail.com>
2023-07-08 10:11:27 +0900

    各バリデーションが正しく動く

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,6 +11,7 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.create(recipe_params)
     if @recipe.save
+      redirect_to root_path
     else
       render :new
     end
